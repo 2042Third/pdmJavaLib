5f151a824809a5981dd58d9eb0ad6841f3ac911e
mike yang <18604713262@163.com>
2022-08-06 17:06:00 -0400

    Android build

--- pdmCryptModule/src/main/cpp/main.cpp
+++ pdmCryptModule/src/main/cpp/main.cpp
@@ -15,3 +15,17 @@
 //         System.loadLibrary("main")
 //      }
 //    }
+
+#include <jni.h>
+#include <iostream>
+extern "C" {
+#include "cc20_wrapper.h"
+    void getLib(){
+        char a[1024] = "hello";
+        char b[1024] ;
+        char c[1024];
+        hash_wrap(a,5,b);
+
+    }
+
+}
