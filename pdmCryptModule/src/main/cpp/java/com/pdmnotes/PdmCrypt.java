6c4b20b6da3f3f87cef73093cd985f7a1855fa7f
mike yang <18604713262@163.com>
2022-08-06 22:06:29 -0400

    new build for all plats

---
+++ pdmCryptModule/src/main/cpp/java/com/pdmnotes/PdmCrypt.java
@@ -0,0 +1,41 @@
+package com.pdmnotes;
+
+import java.util.List;
+
+public class PdmCrypt {
+
+
+    private static final String LIB_NAME = "c20";
+
+    static {
+        // load the native library
+        System.loadLibrary(LIB_NAME);
+    }
+
+    public static void callbackStatic(String string) {
+        System.out.println("Static callback: " + string);
+    }
+
+    // Native method declaration
+    public native String getHash(String a);
+
+    public void callback(String string) {
+        System.out.println("Callback: " + string);
+    }
+
+    public String callbackObject(List<String> arrayList) {
+        System.out.println("Callback object with size: "
+                + arrayList.size()
+                + " and payload: "
+                + arrayList.get(0));
+        return arrayList.get(0);
+    }
+    public String getHashTest(){
+        String a = "hello";
+        return getHash(a);
+    }
+//    public static void main(String[] args) {
+//        PdmCrypt jni = new PdmCrypt();
+//        System.out.println(jni.getHash());
+//    }
+}
